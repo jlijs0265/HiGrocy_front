fb73e05f83bbd70d83bcb899448f89d6315d0461
HuiMun <jlijs0265@gmail.com>
2023-08-22 20:28:53 +0900

    라우터 추가

---
+++ src/route.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Order from "./pages/order";
+import OrderList from "./pages/orderlist";
+import ProductMechine from "./pages/productMechine";
+import Product from "./pages/product";
+import ProductRequest from "./pages/productRequest";
+import Process from "./pages/process";
+import RawMaterial from "./pages/rawMaterial";
+import Storage from "./pages/storage";
+import WareHousing from "./pages/wareHousing";
+
+
+export default function Higorcy_Router() {
+    return (
+      <Router>
+        <div>
+  
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/order">
+              <Order />
+            </Route>
+            <Route path="/orderlist">
+              <OrderList />
+            </Route>
+            <Route path="/pm">
+              <ProductMechine/>
+            </Route>
+            <Route path="/product">
+              <Product/>
+            </Route>
+            <Route path="/production/requestList">
+              <ProductRequest/>
+            </Route>
+            <Route path="/process">
+              <Process/>
+            </Route>
+            <Route path="/pr">
+              <ProductRequest/>
+            </Route>
+            <Route path="/raw_material">
+              <RawMaterial/>
+            </Route>
+            <Route path="/storage">
+              <Storage/>
+            </Route>
+            <Route path="/wh">
+              <WareHousing/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+  
