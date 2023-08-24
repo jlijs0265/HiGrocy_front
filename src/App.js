d532f816556125b82cda86fff078fff066f54a67
HuiMun <jlijs0265@gmail.com>
2023-08-24 21:29:51 +0900

    기본 페이지 수정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 
-import logo from './logo.svg';
 import './App.css';
 import Router from './route';
-import ProfileCard from "./components/ProfileCard";
 
 
 function App() {
   return (
     <div className="App">
       <Router></Router>
-      <ProfileCard />
-
     </div>
   );
 }
