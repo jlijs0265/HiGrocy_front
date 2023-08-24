cce4706ba4f281791afda9cb431e856c70a41cc8
O.Ojinnie <o.ojinnie@gmail.com>
2023-08-24 19:48:19 +0900

    [ADD] Alarm event + login 여부 조건부 렌더링

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,22 +1,44 @@
 import React from "react";
 import "./ProfileCard.css";
-function ProfileCard() {
+import { useState } from "react";
+
+function ProfileCard(props) {
+  const [alarmStat, setAlarmStat] = useState(false);
+
+  props = true;
+
+  //추후 메시지 받으면 이 메서드 사용할 예정
+  // const alarmHandler = (e) => {
+  //   setAlarmStat(true);
+  // };
+
+  const vibration = (e) => {
+    e.target.classList.add("vibration");
+    setAlarmStat(true);
+    console.log("shake~ shake~");
+    setTimeout(() => {
+      e.target.classList.remove("vibration");
+    }, 400);
+  };
+
   return (
     <div className="profile_banner d-flex content-justify-center p-2">
       <div className="profile_list">
-        {/*
-        //로그아웃 상태 시 활성화
-         <img
-          className="profile_banner_user"
-          src="assets/images/unknown.svg"
-          alt="profile_Card_Login_image"
-          style={{ backgroundColor: "white" }}
-        /> */}
-        <img
-          className="profile_banner_user"
-          src="assets/images/avatar.svg"
-          alt="profile_Card_Login_image"
-        />
+        {props ? (
+          <img
+            className="profile_banner_user"
+            src="assets/images/avatar.svg"
+            alt="profile_Card_Login_image"
+          />
+        ) : (
+          <img
+            className="profile_banner_user"
+            src="assets/images/unknown.svg"
+            alt="profile_Card_Login_image"
+            style={{ backgroundColor: "white" }}
+          />
+        )}
+
         <div>
           <div className="profile_bold">Jenny Wilson</div>
 
@@ -33,13 +55,17 @@ function ProfileCard() {
       </div>
       <div className="content-justify-center">
         <img
-          className="profile_banner_alarm m-2"
-          // src="assets/images/alarm.svg"
-          //메시지 수신 시 활성화
-          src="assets/images/alarm_msg.svg"
+          className="profile_banner_alarm m-2 item"
+          src={
+            alarmStat
+              ? "assets/images/alarm_msg.svg"
+              : "assets/images/alarm.svg"
+          }
+          onClick={vibration}
           alt="profile_Card_Alarm_image"
           //로그아웃 상태 시 활성화
-          // style={{ visibility: "hidden" }}
+          style={{ visibility: props ? "visible" : "hidden" }}
+          // style=
         />
         <img
           className="profile_banner_logout m-2"
