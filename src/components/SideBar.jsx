b466f94347fdc0c22beea3ff2a8317d7ee69d8f6
O.Ojinnie <o.ojinnie@gmail.com>
2023-08-25 18:29:40 +0900

    [ADD] SideBar + CSS + Button + Button Effect

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./SideBar.css";
+const SideBar = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleButtonPress = () => {
+    setIsActive(true);
+  };
+
+  const handleButtonRelease = () => {
+    setIsActive(false);
+  };
+  return (
+    <div className="side_style d-flex p-2 justify-content-evenly">
+      <div className={"btn_style"}>
+        <img
+          src="assets/images/higrocyLogo.svg"
+          alt="HiGrocy Button Logo SVG Image"
+        />
+      </div>
+      <div className={"btn_style"}>
+        <img
+          src="assets/images/distribution.svg"
+          alt="HiGrocy Button Logo SVG Image"
+        />
+      </div>
+      <div className={"btn_style"}>
+        <img
+          src="assets/images/order.svg"
+          alt="HiGrocy Button Logo SVG Image"
+        />
+      </div>
+      <div className={"btn_style"}>
+        <img
+          src="assets/images/wareHousing.svg"
+          alt="HiGrocy Button Logo SVG Image"
+        />
+      </div>
+      <div className={"btn_style"}>
+        <img src="assets/images/eco.svg" alt="HiGrocy Button Logo SVG Image" />
+      </div>
+      <div className={"btn_style"}>
+        <img
+          src="assets/images/bricks.svg"
+          alt="HiGrocy Button Logo SVG Image"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
--- src/pages/testpage.jsx
+++ src/pages/testpage.jsx
@@ -1,9 +1,13 @@
+import SideBar from "../components/SideBar";
+
 const TestPage = () => {
-    return(
-        <div>
-            {/* 아래에 테스트할 컴포넌트 입력후 저장, /test로 접속하여 확인 Test 후 git commit시에는 discard change(변경사항 없음)으로 돌려놓고 commit할것  */}
-        </div>
-    );
-}
+  return (
+    <div>
+      {/* 아래에 테스트할 컴포넌트 입력후 저장,
+       /test로 접속하여 확인 Test 후 git commit시에는
+        discard change(변경사항 없음)으로 돌려놓고 commit할것  */}
+    </div>
+  );
+};
 
 export default TestPage;
