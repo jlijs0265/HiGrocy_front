0355ccfac12f08d901993e9bdaf92897d5267638
HuiMun <jlijs0265@gmail.com>
2023-08-27 14:58:12 +0900

    메인화면 layout 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 
 import './App.css';
+import ProfileCard from './components/ProfileCard';
+import SearchBar from './components/SearchBar';
+import SideBar from './components/SideBar';
 import Router from './route';
 
 
 function App() {
   return (
     <div className="App">
-      <Router></Router>
+      <div className='contiainer row'>
+        <div className='col-2 p-4'>
+          <SideBar></SideBar>
+        </div>
+        <div className='col-10'>
+          <div className='d-flex justify-content-between '>
+            <div className='m-3 align-self-center'>
+              <SearchBar></SearchBar>
+            </div>
+            <div className='m-3'>
+              <ProfileCard></ProfileCard>
+            </div>
+          </div>
+          <div className=''>
+            <Router></Router>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
