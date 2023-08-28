0f0caec48c52cb93c27e7f21440a41c8bc7836c1
h2jin <tkdwls1229@naver.com>
2023-08-28 20:53:03 +0900

    form component 추가

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,5 +1,26 @@
-const Form = () => {
-  return <div></div>;
+import Form from 'react-bootstrap/Form';
+import { PropTypes } from "prop-types";
+
+const Form = ({ inputType, label, options }) => {
+  if (inputType === 'input') {
+    return <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+      <Form.Label className="text-start">{label}</Form.Label>
+      <Form.Control type="text" placeholder={label} />
+    </Form.Group>
+  } else if (inputType === 'select') {
+    return <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+      <Form.Label className="text-start">{label}</Form.Label>
+      <Form.Select aria-label="Default select example">
+        {options.map((option, index) => (
+          <option key={index} value={option.value}>{option.label}</option>
+        ))}
+      </Form.Select>
+    </Form.Group>
+  }
 };
+
+Form.propTypes = {
+  inputType : PropTypes.string.isRequired,
+}
 
 export default Form;
