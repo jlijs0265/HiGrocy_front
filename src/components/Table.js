d045563587db42f58b338e38a575080e634bcd87
O.Ojinnie <o.ojinnie@gmail.com>
2023-08-30 18:07:28 +0900

    [TEST] Main Card Setting...

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,29 +1,34 @@
 import { PropTypes } from "prop-types";
 
-const Table = ({ rowNum, colNum, tableCol, trClick, ref}) => {
-
-    return (
-        <div className="table-responsive container">
-            <table className="table table-bordered border-secondary rawTable">
-                <thead>
-                    <tr>
-                        {tableCol.map((value, index) => {return <th key={index}>{value}</th>})}
-                    </tr>
-                </thead>
-                <tbody className="rawbody" onClick={trClick} ref={ref}>
-                    <tr className="rawitem"
-                    >
-                        {Array(colNum).fill(1).map((value, index) => value + index).map((colNum) => { return <td key={colNum}>{colNum}</td> })}
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    );
-}
+const Table = ({ rowNum, colNum, tableCol, trClick, ref }) => {
+  return (
+    <div className="table-responsive container">
+      <table className="table table-bordered border-secondary rawTable">
+        <thead>
+          <tr>
+            {tableCol.map((value, index) => {
+              return <th key={index}>{value}</th>;
+            })}
+          </tr>
+        </thead>
+        <tbody className="rawbody" onClick={trClick} ref={ref}>
+          <tr className="rawitem">
+            {Array(colNum)
+              .fill(1)
+              .map((value, index) => value + index)
+              .map((colNum) => {
+                return <td key={colNum}>{colNum}</td>;
+              })}
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 Table.defaultProps = {
-    rowNum: 5,
-    colNum: 5,
-}
+  rowNum: 5,
+  colNum: 5,
+};
 
 export default Table;
--- src/pages/mainpage.jsx
+++ src/pages/mainpage.jsx
@@ -1,21 +1,24 @@
 import AreaChart from "../components/AreaChart";
 import BarChart from "../components/BarChart";
 import LineChart from "../components/LineChart";
+import "../components/Card.css";
 
 const Mainpage = () => {
-    return (
-        <div className="contain-fluid page-body-wrapper">
-            <div className="">
-                <LineChart />
-            </div>
-            <div className="">
-                <AreaChart />
-            </div>
-            <div className="">
-                <BarChart />
-            </div>
-        </div>
-    );
-}
+  return (
+    // <div className="container d-flex flex-wrap align-items-center">
+    <div className="card-body d-flex">
+      <div className="m-2 card-main">
+        <LineChart />
+      </div>
+      <div className="m-2 card-main">
+        <BarChart />
+      </div>
+      <div className="m-2 card-main">
+        <AreaChart />
+      </div>
+    </div>
+    // </div>
+  );
+};
 
 export default Mainpage;
