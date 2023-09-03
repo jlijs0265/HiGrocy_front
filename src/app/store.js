a4e4ca1afd841aff8dd4c9b93c241df1a86f023c
HuiMun <jlijs0265@gmail.com>
2023-09-03 16:37:33 +0900

    등록, 수정-삭제 버튼 관련하여 redux 처리

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import processReducer from '../features/process/processSlice'
+import generalFormReducer from '../features/process/generalFormSlice'
 
 export const store = configureStore({
   reducer: {
     todos: processReducer,
+    generalFrom: generalFormReducer,
+  
   }
 })
---
+++ src/features/process/generalFormSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const generalFormSlice = createSlice({
+  name: 'FormControl',
+  initialState: {
+    register : true
+  },
+  reducers: {
+    registerForm(state, action) {
+      state.register = true;
+    },
+    modifyForm(state, action) {
+      state.register = false;
+    }
+  }
+})
+
+export const { registerForm, modifyForm } = generalFormSlice.actions
+export default generalFormSlice.reducer
