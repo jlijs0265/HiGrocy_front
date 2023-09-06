7d5e2bdd96311d4ba108202f43564be98bdff019
HuiMun <jlijs0265@gmail.com>
2023-09-06 15:08:02 +0900

    주석으로 설명추가

--- src/app/generalTriggerSlice.js
+++ src/app/generalTriggerSlice.js
@@ -1,4 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit'
+/*
+  페이지 화면에서 Boolean값으로 처리해야하는 State를 모아둠.
+  register : form내에 등록 / 수정,삭제 버튼 활성화
+  modal : 모달창 자체 활성화
+  modalType : 모달창 종류(/component/modals.js 참고)
+*/
 
 const generalTriggerSlice = createSlice({
   name: 'trigger',
@@ -8,12 +14,15 @@ const generalTriggerSlice = createSlice({
     modalType : ''
   },
   reducers: {
+    // 등록버튼 활성화
     registerForm(state, action) {
       state.register = true;
     },
+    // 수정, 삭제 버튼 활성화
     modifyForm(state, action) {
       state.register = false;
     },
+    // 모달창 타입설정, 활성화 
     toggleModal(state, action) {
       state.modalType = action.payload;
       state.modal = !state.modal;
--- src/app/store.js
+++ src/app/store.js
@@ -3,14 +3,19 @@ import processReducer from './processSlice'
 import generalTriggerReducer from './generalTriggerSlice'
 import ModalReducer from './modalSlice'
 
+/*
+  redux 사용하기위한 설정파일
+  react 내에서 init 되면 아래 reducer 객체 이름으로 state값이 initstate값으로 초기화됨.
+*/
+
 export const store = configureStore({
-  // redux가 react 내에서 init 되면 아래 reducer 객체 이름으로 state값이 initstate값으로 초기화됨.
   reducer: {
     process: processReducer,
     generalTrigger: generalTriggerReducer,
     modal: ModalReducer,
   
   },
+  //action.payload 값이 원래 serializable 한 값으로 전송되어야함(객체, 리스트) tool-kit사용시 아닌값도 전송할수 있는데, 그때 에러나는 에러를 무시하기위한 설정
   middleware: getDefaultMiddleware => getDefaultMiddleware({ serializableCheck: false }),
 
 })
